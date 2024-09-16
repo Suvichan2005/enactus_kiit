cdae1b5ff35d6314f388be60886243ff47ef8063
Suvichan2005 <suvanshagar@gmail.com>
2024-09-16 18:41:40 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart'; // Import flutter_svg package
 import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // Import FontAwesome
+import 'package:url_launcher/url_launcher.dart'; // Import url_launcher package
 
 void main() {
   runApp(const MyApp());
@@ -26,13 +28,19 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black, // Set the background color of the Scaffold
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: Row(
           children: [
             Image.asset(
-              'assets/enactus.png', // Your logo image
+              'web/enactus.png', // Your logo image
               height: 40,
+            ),
+            const SizedBox(width: 10),
+            const Text(
+              'enactus KIIT-KISS',
+              style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
             ),
             const Spacer(),
             _buildNavButton("Home"),
@@ -44,136 +52,178 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: SingleChildScrollView(
-  child: Container(
-    decoration: BoxDecoration(
-      color: Colors.black, // Set the background color to black
-    ),
-    child: Column(
-      children: [
-        // Right-side graphic elements
-        Positioned(
-          right: 0,
-          child: Image.asset(
-            'assets/graphic_vectors.png', // Your graphic vector image
-            height: MediaQuery.of(context).size.height * 0.7,
-          ),
-        ),
-        // Main content
-        Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'enactus',
-                style: TextStyle(
-                  fontSize: 64,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+        child: Stack(
+          children: [
+            Positioned(
+              right: 0,
+              child: SvgPicture.asset(
+                'web/triangle.bd4f7212.svg', // Your SVG image file path
+                height: MediaQuery.of(context).size.height * 0.7,
+                placeholderBuilder: (BuildContext context) => Container(
+                  width: 50,
+                  height: 50,
+                  child: const CircularProgressIndicator(),
                 ),
               ),
-              const Text(
-                'KIIT-KISS',
-                style: TextStyle(
-                  fontSize: 32,
-                  color: Colors.yellow,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Enactus is the world\'s largest experiential\n'
-                'learning platform dedicated to creating a better world\n'
-                'developing the next generation of entrepreneurial\n'
-                'leaders and social innovators...',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  height: 1.5, // Increase line height for readability
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.yellow,
-                  foregroundColor: Colors.black,
-                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                ),
-                child: const Text('Read More'),
-              ),
-              const SizedBox(height: 40),
-              // Social Media Icons using FontAwesome
-              Row(
+            ),
+            Container(
+              color: Colors.black, // Set the background color of the main content
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  _buildSocialIcon(FontAwesomeIcons.facebook),
-                  _buildSocialIcon(FontAwesomeIcons.linkedin),
-                  _buildSocialIcon(FontAwesomeIcons.instagram),
+                  const SelectableText(
+                    'enactus',
+                    style: TextStyle(
+                      fontSize: 64,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.black,
+                          offset: Offset(5.0, 5.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SelectableText(
+                    'KIIT-KISS',
+                    style: TextStyle(
+                      fontSize: 32,
+                      color: Colors.yellow,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.black,
+                          offset: Offset(5.0, 5.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const SelectableText(
+                    'Enactus is the world\'s largest experiential\n'
+                    'learning platform dedicated to creating a better world\n'
+                    'developing the next generation of entrepreneurial\n'
+                    'leaders and social innovators...',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      height: 1.5, // Increase line height for readability
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.black,
+                          offset: Offset(5.0, 5.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.yellow,
+                      foregroundColor: Colors.black,
+                      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                    ),
+                    child: const Text('Read More'),
+                  ),
+                  const SizedBox(height: 40),
+                  // Social Media Icons using FontAwesome
+                  Row(
+                    children: [
+                      _buildSocialIcon(FontAwesomeIcons.facebook, 'https://www.facebook.com/kiitenactus/'),
+                      _buildSocialIcon(FontAwesomeIcons.linkedin, 'https://www.linkedin.com/company/enactus-kiit'),
+                      _buildSocialIcon(FontAwesomeIcons.instagram, 'https://www.instagram.com/enactus.kiit/'),
+                    ],
+                  ),
+                  const SizedBox(height: 40),
+                  // "What we believe" section
+                  const SelectableText(
+                    'What we believe in?',
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.black,
+                          offset: Offset(5.0, 5.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const SelectableText(
+                    'Enactus is a nonprofit organization that was founded in the year 1965. It is led\n'
+                    'by student teams from all the top universities in the world which creates\n'
+                    'entrepreneurial projects to address social, environmental, and economic\n'
+                    'issues in order to achieve the sustainable development goals in the\n'
+                    'community.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      height: 1.5, // Increase line height for readability
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.black,
+                          offset: Offset(5.0, 5.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildBeliefIcon(FontAwesomeIcons.handshake, 'Integrity'),
+                      _buildBeliefIcon(FontAwesomeIcons.lightbulb, 'Innovation'),
+                      _buildBeliefIcon(FontAwesomeIcons.users, 'Collaboration'),
+                    ],
+                  ),
+                  const SizedBox(height: 40),
                 ],
               ),
-              const SizedBox(height: 40),
-              // "What we believe" section
-              const Text(
-                'What we believe',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Enactus is a nonprofit organization that was founded in the year 1965. It is led\n'
-                'by student teams from all the top universities in the world which creates\n'
-                'entrepreneurial projects to address social, environmental, and economic\n'
-                'issues in order to achieve the sustainable development goals in the\n'
-                'community.',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  height: 1.5, // Increase line height for readability
-                ),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildBeliefIcon(FontAwesomeIcons.handshake, 'Integrity'),
-                  _buildBeliefIcon(FontAwesomeIcons.lightbulb, 'Innovation'),
-                  _buildBeliefIcon(FontAwesomeIcons.users, 'Collaboration'),
-                ],
-              ),
-              const SizedBox(height: 40),
-            ],
-          ),
+            ),
+          ],
         ),
-      ],
-    ),
-  ),
-),
-);
-
+      ),
+    );
   }
 
   Widget _buildNavButton(String title) {
     return TextButton(
       onPressed: () {},
-      child: Text(
-        title,
-        style: const TextStyle(color: Colors.white),
-      ),
       style: ButtonStyle(
         overlayColor: MaterialStateProperty.all(Colors.yellow.withOpacity(0.2)),
+      ),
+      child: Text(
+        title,
+        style: const TextStyle(color: Colors.white, fontSize: 16),
       ),
     );
   }
 
-  Widget _buildSocialIcon(IconData icon) {
+  Widget _buildSocialIcon(IconData icon, String url) {
     return IconButton(
       icon: FaIcon(icon, color: Colors.yellow),
-      onPressed: () {},
+      onPressed: () => _launchURL(url),
       hoverColor: Colors.yellow.withOpacity(0.2),
     );
+  }
+
+  void _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 
   Widget _buildBeliefIcon(IconData icon, String label) {
